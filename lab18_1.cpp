be9c9cf4f3dbd92816b6914f3e8c8d22f58f9731
Kotaro <kota.kawaka@gmail.com_>
2025-02-07 15:26:34 +0700

    lab18.1 finish

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,12 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
 
-
-
+double overlap(Rect R1,Rect R2){
+	double out;
+	double xs,ys ;
+	xs = max(0.0,min(R1.w+R1.x,R2.w+R2.x) -max(R1.x,R2.x));
+	ys = max(0.0, min(R1.y,R2.y)-max(R1.y-R1.h,R2.y-R2.h));
+	out = xs * ys;
+	return out;
 }
